570f64f9247de6a25be466356ae684ed85230a7c
andrew brown <dr.andrew.david.brown@gmail.com>
2023-10-16 09:10:37 +0000

    implement js for sidebar menu

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,11 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    //initialze sidenav
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav);
+  });*/
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+});
