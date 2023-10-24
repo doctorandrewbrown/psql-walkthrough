8490346a17a7e1c29fd5d157ecd8dff10203fbd5
andrew brown <dr.andrew.david.brown@gmail.com>
2023-10-24 09:52:56 +0000

    add comments

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -11,9 +11,12 @@ def home():
 @app.route("/categories")
 def categories():
     # convert cursor object returned by query into python list
+    # NB each element of list represents a row of fields of model
+    #row fields are accessed via dot notation
     categories = list(Category.query.all())
-    # categories argument in render_template() is variable name passed to template. It
-    # is assigned to value of categories from route
+
+    # categories argument in render_template() is variable name passed to 
+    # template. It is assigned to value of categories as defined above
     return render_template("categories.html", categories=categories)
 
 
@@ -26,3 +29,9 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    return render_template("edit_category.html", category=category)
