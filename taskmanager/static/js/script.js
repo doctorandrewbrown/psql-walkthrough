39e0c7c08c7fa8f81f7dd69988f71aa984126c67
andrew brown <dr.andrew.david.brown@gmail.com>
2023-11-05 14:18:02 +0000

    initialize collapsible

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -15,4 +15,9 @@ document.addEventListener("DOMContentLoaded", function() {
   // select initialization
   let selects = document.querySelectorAll("select");
   M.FormSelect.init(selects);
+
+  // collapsible initialization
+  let elems = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(elems);
+
 });
